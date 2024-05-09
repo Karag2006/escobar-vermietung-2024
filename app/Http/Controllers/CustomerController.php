70ae0b2a8806c27c59ae3e2adbf711c1fcd27422
Martin Richter <karag2006@gmx.net>
2024-05-09 09:05:28 +0200

    Store Customer Backend.

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -21,19 +21,11 @@ class CustomerController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreCustomerRequest $request)
     {
-        //
+        $customer = Customer::create($request->all());
     }
 
     /**
--- routes/customer.php
+++ routes/customer.php
@@ -3,4 +3,6 @@ use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/customer', [CustomerController::class, 'index'])->name('customer'); // List
+Route::post('/customer', [CustomerController::class, 'store'])->name('customer.store');
+
 ?>
