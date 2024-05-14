9ca7c1dc1540896164cff3e90c0e100f06f99770
Martin Richter <karag2006@gmx.net>
2024-05-14 15:36:32 +0200

    init + global Data Table

--- resources/js/Components/data-table/pagination.tsx
+++ resources/js/Components/data-table/pagination.tsx
@@ -14,6 +14,11 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/Components/ui/select";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+} from "@/Components/ui/tooltip";
 
 interface DataTablePaginationProps<TData> {
     table: Table<TData>;
@@ -27,20 +32,30 @@ export function DataTablePagination<TData>({
             <div className="w-full flex items-center justify-end space-x-6 lg:space-x-8">
                 <div>
                     <div className="flex items-center space-x-2">
-                        <p className="text-sm font-medium">Zeilen pro Seite</p>
+                        <p className="text-sm font-medium">
+                            Zeilen pro Seite :{" "}
+                        </p>
                         <Select
                             value={`${table.getState().pagination.pageSize}`}
                             onValueChange={(value) => {
                                 table.setPageSize(Number(value));
                             }}
                         >
-                            <SelectTrigger className="h-8 w-[70px]">
-                                <SelectValue
-                                    placeholder={
-                                        table.getState().pagination.pageSize
-                                    }
-                                />
-                            </SelectTrigger>
+                            <Tooltip>
+                                <TooltipTrigger asChild>
+                                    <SelectTrigger className="h-8 w-[70px]">
+                                        <SelectValue
+                                            placeholder={
+                                                table.getState().pagination
+                                                    .pageSize
+                                            }
+                                        />
+                                    </SelectTrigger>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    Zeilen pro Seite ändern
+                                </TooltipContent>
+                            </Tooltip>
                             <SelectContent side="top">
                                 {[10, 20, 30, 40, 50].map((pageSize) => (
                                     <SelectItem
@@ -60,44 +75,72 @@ export function DataTablePagination<TData>({
                         {table.getPageCount()}
                     </div>
                     <div className="flex items-center space-x-2">
-                        <Button
-                            variant="outline"
-                            className="hidden h-8 w-8 p-0 lg:flex"
-                            onClick={() => table.setPageIndex(0)}
-                            disabled={!table.getCanPreviousPage()}
-                        >
-                            <span className="sr-only">erste Seite</span>
-                            <ChevronsLeftIcon className="h-4 w-4" />
-                        </Button>
-                        <Button
-                            variant="outline"
-                            className="h-8 w-8 p-0"
-                            onClick={() => table.previousPage()}
-                            disabled={!table.getCanPreviousPage()}
-                        >
-                            <span className="sr-only">vorherige Seite</span>
-                            <ChevronLeftIcon className="h-4 w-4" />
-                        </Button>
-                        <Button
-                            variant="outline"
-                            className="h-8 w-8 p-0"
-                            onClick={() => table.nextPage()}
-                            disabled={!table.getCanNextPage()}
-                        >
-                            <span className="sr-only">nächste Seite</span>
-                            <ChevronRightIcon className="h-4 w-4" />
-                        </Button>
-                        <Button
-                            variant="outline"
-                            className="hidden h-8 w-8 p-0 lg:flex"
-                            onClick={() =>
-                                table.setPageIndex(table.getPageCount() - 1)
-                            }
-                            disabled={!table.getCanNextPage()}
-                        >
-                            <span className="sr-only">letzte Seite</span>
-                            <ChevronsRightIcon className="h-4 w-4" />
-                        </Button>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Button
+                                    variant="outline"
+                                    className="hidden h-8 w-8 p-0 lg:flex"
+                                    onClick={() => table.setPageIndex(0)}
+                                    disabled={!table.getCanPreviousPage()}
+                                >
+                                    <span className="sr-only">erste Seite</span>
+                                    <ChevronsLeftIcon className="h-4 w-4" />
+                                </Button>
+                            </TooltipTrigger>
+                            <TooltipContent>erste Seite</TooltipContent>
+                        </Tooltip>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Button
+                                    variant="outline"
+                                    className="h-8 w-8 p-0"
+                                    onClick={() => table.previousPage()}
+                                    disabled={!table.getCanPreviousPage()}
+                                >
+                                    <span className="sr-only">
+                                        vorherige Seite
+                                    </span>
+                                    <ChevronLeftIcon className="h-4 w-4" />
+                                </Button>
+                            </TooltipTrigger>
+                            <TooltipContent>vorherige Seite</TooltipContent>
+                        </Tooltip>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Button
+                                    variant="outline"
+                                    className="h-8 w-8 p-0"
+                                    onClick={() => table.nextPage()}
+                                    disabled={!table.getCanNextPage()}
+                                >
+                                    <span className="sr-only">
+                                        nächste Seite
+                                    </span>
+                                    <ChevronRightIcon className="h-4 w-4" />
+                                </Button>
+                            </TooltipTrigger>
+                            <TooltipContent>nächste Seite</TooltipContent>
+                        </Tooltip>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Button
+                                    variant="outline"
+                                    className="hidden h-8 w-8 p-0 lg:flex"
+                                    onClick={() =>
+                                        table.setPageIndex(
+                                            table.getPageCount() - 1
+                                        )
+                                    }
+                                    disabled={!table.getCanNextPage()}
+                                >
+                                    <span className="sr-only">
+                                        letzte Seite
+                                    </span>
+                                    <ChevronsRightIcon className="h-4 w-4" />
+                                </Button>
+                            </TooltipTrigger>
+                            <TooltipContent>letzte Seite</TooltipContent>
+                        </Tooltip>
                     </div>
                 </div>
             </div>
--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -20,6 +20,7 @@ import { Separator } from "@/Components/ui/separator";
 import { NavMenu } from "./components/nav-menu";
 import { CardWrapper } from "@/Components/wrapper/card-wrapper";
 import { PageTitle } from "@/Components/page-title";
+import { TooltipProvider } from "@/Components/ui/tooltip";
 
 interface AuthenticatedLayoutProps extends PropsWithChildren {
     user: User;
@@ -55,95 +56,101 @@ export default function Authenticated({
 
     return (
         <div className="min-h-screen w-full">
-            <Drawer direction="left">
-                <div className="flex w-full">
-                    <div className="min-h-screen hidden sm:block bg-accent">
-                        <NavMenu className="w-full max-w-[350px]" />
-                    </div>
-                    <div className="w-full flex flex-col items-center">
-                        <div className="p-4 mb-4 bg-white flex w-full items-center justify-between shadow-md">
-                            <DrawerTrigger className="block sm:hidden m-4">
-                                <Menu className="h-6 w-6" />
-                                <span className="sr-only">Open Nav Menu</span>
-                            </DrawerTrigger>
-                            <p className="font-semibold text-xl">
-                                Escobar Anhängercenter
-                            </p>
-
-                            <DropdownMenu>
-                                <DropdownMenuTrigger asChild>
-                                    <Button
-                                        variant="primary"
-                                        size="sm"
-                                        className="inline-flex"
-                                    >
-                                        <UserIcon className="h-6 w-6 mr-2" />
-                                        {user.name}
-                                    </Button>
-                                </DropdownMenuTrigger>
-                                <DropdownMenuContent>
-                                    <DropdownMenuItem asChild>
-                                        <Link
-                                            href={route("profile.edit")}
-                                            className="flex gap-4"
-                                        >
-                                            <KeyRound className="h-6 w-6" />
-                                            <span className="inline-block">
-                                                Profile
-                                            </span>
-                                        </Link>
-                                    </DropdownMenuItem>
-                                    <DropdownMenuItem asChild>
-                                        <Link
-                                            href={route("logout")}
-                                            method="post"
-                                            as="button"
-                                            className="flex gap-4"
-                                        >
-                                            <Power className="h-6 w-6" />
-                                            <span className="inline-block">
-                                                Logout
-                                            </span>
-                                        </Link>
-                                    </DropdownMenuItem>
-                                </DropdownMenuContent>
-                            </DropdownMenu>
+            <TooltipProvider>
+                <Drawer direction="left">
+                    <div className="flex w-full">
+                        <div className="min-h-screen hidden sm:block bg-accent">
+                            <NavMenu className="w-full max-w-[350px]" />
                         </div>
-                        <CardWrapper
-                            header={
-                                headerAction ? (
-                                    <PageTitle title={header}>
-                                        {headerAction}
-                                    </PageTitle>
-                                ) : (
-                                    <PageTitle title={header} />
-                                )
-                            }
-                            showHeader={header ? true : false}
-                            footer={footer}
-                        >
-                            <main>{children}</main>
-                        </CardWrapper>
-                    </div>
-                </div>
+                        <div className="w-full flex flex-col items-center">
+                            <div className="p-4 mb-4 bg-white flex w-full items-center justify-between shadow-md">
+                                <DrawerTrigger className="block sm:hidden m-4">
+                                    <Menu className="h-6 w-6" />
+                                    <span className="sr-only">
+                                        Open Nav Menu
+                                    </span>
+                                </DrawerTrigger>
+                                <p className="font-semibold text-xl">
+                                    Escobar Anhängercenter
+                                </p>
 
-                <DrawerContent className="bg-accent text-accent-foreground min-h-screen max-w-[280px] flex flex-col sm:hidden">
-                    <div className="relative">
-                        <DrawerClose className="absolute top-[116px] -right-6">
-                            <Button
-                                variant="accent"
-                                className="pl-0 pr-0 h-20 rounded-tl-none rounded-bl-none "
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger asChild>
+                                        <Button
+                                            variant="primary"
+                                            size="sm"
+                                            className="inline-flex"
+                                        >
+                                            <UserIcon className="h-6 w-6 mr-2" />
+                                            {user.name}
+                                        </Button>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent>
+                                        <DropdownMenuItem asChild>
+                                            <Link
+                                                href={route("profile.edit")}
+                                                className="flex gap-4"
+                                            >
+                                                <KeyRound className="h-6 w-6" />
+                                                <span className="inline-block">
+                                                    Profile
+                                                </span>
+                                            </Link>
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem asChild>
+                                            <Link
+                                                href={route("logout")}
+                                                method="post"
+                                                as="button"
+                                                className="flex gap-4"
+                                            >
+                                                <Power className="h-6 w-6" />
+                                                <span className="inline-block">
+                                                    Logout
+                                                </span>
+                                            </Link>
+                                        </DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                            </div>
+                            <CardWrapper
+                                header={
+                                    headerAction ? (
+                                        <PageTitle title={header}>
+                                            {headerAction}
+                                        </PageTitle>
+                                    ) : (
+                                        <PageTitle title={header} />
+                                    )
+                                }
+                                showHeader={header ? true : false}
+                                footer={footer}
                             >
-                                <ChevronLeft className="h-6 w-6" />
-                                <span className="sr-only">Close Nav Menu</span>
-                            </Button>
-                        </DrawerClose>
-                        <ApplicationLogo />
-                        <Separator className="w-full my-2" />
+                                <main>{children}</main>
+                            </CardWrapper>
+                        </div>
                     </div>
-                    <NavMenu />
-                </DrawerContent>
-            </Drawer>
+
+                    <DrawerContent className="bg-accent text-accent-foreground min-h-screen max-w-[280px] flex flex-col sm:hidden">
+                        <div className="relative">
+                            <DrawerClose className="absolute top-[116px] -right-6">
+                                <Button
+                                    variant="accent"
+                                    className="pl-0 pr-0 h-20 rounded-tl-none rounded-bl-none "
+                                >
+                                    <ChevronLeft className="h-6 w-6" />
+                                    <span className="sr-only">
+                                        Close Nav Menu
+                                    </span>
+                                </Button>
+                            </DrawerClose>
+                            <ApplicationLogo />
+                            <Separator className="w-full my-2" />
+                        </div>
+                        <NavMenu />
+                    </DrawerContent>
+                </Drawer>
+            </TooltipProvider>
         </div>
     );
 }
