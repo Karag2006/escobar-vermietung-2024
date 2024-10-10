88a52289412337919f4a18cce59cdca3d83cea4d
Martin <karag2006@gmx.net>
2024-10-10 12:05:01 +0200

    Added tooltip to display holiday Names.

--- resources/js/Pages/ReservationTable/components/CalendarDay.tsx
+++ resources/js/Pages/ReservationTable/components/CalendarDay.tsx
@@ -1,7 +1,12 @@
+import Holidays from "date-holidays";
 import { format, isWeekend } from "date-fns";
 
 import { cn } from "@/lib/utils";
-import Holidays from "date-holidays";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+} from "@/Components/ui/tooltip";
 
 interface CalendarDayProps {
     day: Date;
@@ -10,8 +15,10 @@ interface CalendarDayProps {
 
 export const CalendarDay = ({ day, trailerId }: CalendarDayProps) => {
     const hd = new Holidays("DE", "RP");
-    const dayNumber = format(day, "d");
     const holiday = hd.isHoliday(day);
+    const isHoliday = holiday && holiday[0].type === "public" ? true : false;
+
+    const dayNumber = format(day, "d");
     return (
         <div
             className={cn(
@@ -21,19 +28,37 @@ export const CalendarDay = ({ day, trailerId }: CalendarDayProps) => {
             )}
         >
             {dayNumber}
-            <div
-                className={cn(
-                    "absolute top-0 left-0 flex w-full h-full",
-                    holiday && holiday[0].type === "public"
-                        ? "bg-red-500/15"
-                        : "",
-                    isWeekend(day) ? "bg-slate-500/15" : ""
-                )}
-            >
-                <div className="w-full"></div>
-                <div className="w-full"></div>
-                <div className="w-full"></div>
-            </div>
+            {isHoliday ? (
+                <Tooltip>
+                    <TooltipTrigger>
+                        <div
+                            className={cn(
+                                "absolute top-0 left-0 flex w-full h-full",
+                                isHoliday ? "bg-red-500/15" : "",
+                                isWeekend(day) ? "bg-slate-500/15" : ""
+                            )}
+                        >
+                            <div className="w-full"></div>
+                            <div className="w-full"></div>
+                            <div className="w-full"></div>
+                        </div>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                        <p>{holiday && holiday[0].name}</p>
+                    </TooltipContent>
+                </Tooltip>
+            ) : (
+                <div
+                    className={cn(
+                        "absolute top-0 left-0 flex w-full h-full",
+                        isWeekend(day) ? "bg-slate-500/15" : ""
+                    )}
+                >
+                    <div className="w-full"></div>
+                    <div className="w-full"></div>
+                    <div className="w-full"></div>
+                </div>
+            )}
         </div>
     );
 };
