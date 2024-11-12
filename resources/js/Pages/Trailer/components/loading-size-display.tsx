fb1a5c9fadd94395751e0bd280a5d618f0df8185
Martin <karag2006@gmx.net>
2024-11-12 08:13:50 +0100

    fixed: Trailer -> loadingSizeDisplay

--- resources/js/Pages/Trailer/components/loading-size-display.tsx
+++ resources/js/Pages/Trailer/components/loading-size-display.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 interface LoadingSizeDisplayProps {
     loadingSize?: string | null;
     // loadingSize is expected be the string representation of an array with the form:
@@ -10,13 +12,26 @@ export const LoadingSizeDisplay = ({
     loadingSize,
     className,
 }: LoadingSizeDisplayProps) => {
-    let displayString = null;
-    if (loadingSize) {
-        displayString = `${loadingSize[0]} x ${loadingSize[1]}`;
-        if (loadingSize[2])
-            displayString = displayString + ` x ${loadingSize[2]}`;
+    const [displayString, setDisplayString] = useState("");
+    let loadingSizeArray: string[] = [];
+
+    const createDisplayString = (loadingSize: string) => {
+        let displayString = "";
+        loadingSizeArray = JSON.parse(loadingSize);
+        displayString = `${loadingSizeArray[0]} x ${loadingSizeArray[1]}`;
+        if (loadingSizeArray[2])
+            displayString = displayString + ` x ${loadingSizeArray[2]}`;
 
         displayString = displayString + " cm";
-    }
+
+        return displayString;
+    };
+
+    useEffect(() => {
+        if (loadingSize) {
+            setDisplayString(createDisplayString(loadingSize));
+        }
+    }, [loadingSize]);
+
     return <span className={className}>{displayString}</span>;
 };
