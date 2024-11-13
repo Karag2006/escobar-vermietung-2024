99679df9cfe24a3bde412c01a012a46eced944d3
Martin Richter <karag2006@gmx.net>
2024-11-13 16:24:58 +0100

    Update Database Structure.

---
+++ app/Console/Commands/Update2024.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Update2024 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tp24:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update the Database from 2022 version to the 2024 version (this version)';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Updating the Database from 2022 version to 2024 version...');
+
+        $this->call('migrate', [
+            '--path' => 'database/migrations/update_2024',
+        ]);
+
+        $this->call('db:seed', [
+            '--class' => 'Update2024Seeder',
+        ]);
+
+        $this->info('Database updated successfully.');
+    }
+}
