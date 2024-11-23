45931226f442e316ce0ae75c5396e64866479da3
Martin Richter <karag2006@gmx.net>
2024-11-23 10:40:09 +0100

    Update DocumentController.php and collision-dialog.tsx

--- resources/js/Pages/Document/components/collision-dialog.tsx
+++ resources/js/Pages/Document/components/collision-dialog.tsx
@@ -3,6 +3,7 @@ import { Modal } from "@/Components/wrapper/modal";
 import { ModalCardWrapper } from "@/Components/wrapper/modal-card-wrapper";
 import { getDocumentTypeTranslation } from "@/lib/utils";
 import { collisionData } from "@/types/document";
+import { AlertTriangle } from "lucide-react";
 
 interface CollisionDialogProps {
     id: number;
@@ -21,44 +22,48 @@ export const CollisionDialog = ({
         <Modal modalOpen={true} openChange={setCollisionDialog}>
             <ModalCardWrapper
                 header={
-                    <h3 className="font-semibold text-xl text-gray-800">
-                        Potenzielle Überschneidung!
-                    </h3>
+                    <div className="flex gap-4 items-center">
+                        <AlertTriangle className="w-8 h-8 text-red-500" />
+                        <h3 className="font-semibold text-xl text-gray-800">
+                            Achtung Überschneidung!
+                        </h3>
+                    </div>
                 }
                 showHeader
-                footer={
-                    <DecisionButtons
-                        yesLabel="Trotzdem Speichern"
-                        noLabel="Abbrechen"
-                        id={id}
-                        yesAction={confirmCollision}
-                        noAction={() => setCollisionDialog(false)}
-                    />
-                }
             >
-                <p className=" font-bold">
-                    Details : <br />
-                    {`${getDocumentTypeTranslation(
-                        collision.documentType
-                    )} Nummer ${collision.documentNumber}`}
-                    <br />
-                    {`Kunde : ${collision.customerName}`}
-                    <br />
-                    {`Von : ${collision.startDate} - ${collision.startTime}`}
-                    <br />
-                    {`Bis : ${collision.endDate} - ${collision.endTime}`}
-                    <br />
-                    {collision.reservationFeePayed ? (
-                        <span>
-                            {`Anzahlung gezahlt am ${collision.reservationFeeDate}`}
-                        </span>
-                    ) : (
-                        <span>
-                            {`Anzahlung noch offen, Zahlung angegeben bis ${collision.reservationFeeDate}`}
-                        </span>
-                    )}
-                </p>
-                Das Dokument kann trotzdem gespeichert werden.
+                <div className="flex flex-col gap-4 font-bold">
+                    <p>
+                        Details : <br />
+                        {`${getDocumentTypeTranslation(
+                            collision.documentType
+                        )} Nummer ${collision.documentNumber}`}
+                        <br />
+                        {`Kunde : ${collision.customerName}`}
+                        <br />
+                        {`Von : ${collision.startDate} - ${collision.startTime}`}
+                        <br />
+                        {`Bis : ${collision.endDate} - ${collision.endTime}`}
+                        <br />
+                        {collision.reservationFeePayed &&
+                        collision.reservationFeeDate ? (
+                            <span>
+                                {`Anzahlung gezahlt am ${collision.reservationFeeDate}`}
+                            </span>
+                        ) : collision.reservationFeeDate ? (
+                            <span>
+                                {`Anzahlung noch offen, Zahlung angegeben bis ${collision.reservationFeeDate}`}
+                            </span>
+                        ) : (
+                            ""
+                        )}
+                    </p>
+
+                    <p>
+                        Mögliche Lösungen:
+                        <br />
+                        Anhänger Wechseln:
+                    </p>
+                </div>
             </ModalCardWrapper>
         </Modal>
     );
