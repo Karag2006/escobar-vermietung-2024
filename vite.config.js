1d5d7a35cba1d08a366f01cac9d31861e4381ba6
Martin Richter <karag2006@gmx.net>
2024-11-27 11:53:16 +0100

    Update package.json, vite.config.js, and yarn.lock

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    build: {
+        lib: {
+            entry: "resources/js/app.tsx",
+            name: "app",
+        },
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
-            ssr: 'resources/js/ssr.tsx',
+            input: "resources/js/app.tsx",
+            ssr: "resources/js/ssr.tsx",
             refresh: true,
         }),
         react(),
