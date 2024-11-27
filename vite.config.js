8911725ed843a4646c0ac870c39684828733f6df
Martin Richter <karag2006@gmx.net>
2024-11-27 12:02:19 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
     build: {
         lib: {
             entry: "resources/js/app.tsx",
             name: "app",
+        },
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "resources/js/app.tsx"),
+            },
         },
     },
     plugins: [
